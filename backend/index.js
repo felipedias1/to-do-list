7bc04d776ee583f0ff4ac828de0246394895057a
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-15 12:03:36 -0300

    update: import errorMiddleware on index.js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const userRoutes = require('./routes/userRoutes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 app.use(express.json());
 
 app.use('/user', userRoutes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(errorMiddleware.error);
