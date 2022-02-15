55264a7d5da2a9ed6d4ed7485d7e8fb3a633af6b
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-15 11:45:20 -0300

    update: add rules at layer userService

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,6 +1,19 @@
 const userModel = require('../models/userModel');
+const JOI = require('@hapi/joi');
+const { errorUtils } = require('./utils');
+
+const userSchema = JOI.object({
+  name: JOI.string().required().min(8), 
+  email: JOI.string().email().required(), 
+  password: JOI.string().required().length(6),
+});
 
 const newUserService = async (newUser) => {
+  const { name, email, password } = newUser
+
+  const validate = userSchema.validate({ name, email, password });
+  if (validate.error) throw errorUtils(400, validate.error.message);
+
   const createUser = await userModel.newUserMod(newUser);
   return createUser;
 };
