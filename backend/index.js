0646132b2ed7282b989a2cfbcbf0698a0844b536
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-15 10:06:03 -0300

    build: create user route

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/user', userRoutes)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
