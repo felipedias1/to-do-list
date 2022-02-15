cd3065b0a56581496f5831f485207bfec3cd2095
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-15 18:08:37 -0300

    update: add new rule to taskService

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -3,10 +3,10 @@ const taskService = require('../services/taskService');
 const newTaskController = async (req, res, next) => {
 	try {
 		const newTask = req.body;
-		const { id: userId } = req.user;
+		const { id: userId, email } = req.user;
 		console.log(req.user);
 		console.log(newTask);
-		const createTask = await taskService.newTaskService(newTask, userId);
+		const createTask = await taskService.newTaskService(newTask, userId, email);
 		return res.status(201).json({ message: 'Task was created', _id: createTask});
 	} catch (error) {
 		console.log(error.message);
--- backend/services/taskService.js
+++ backend/services/taskService.js
@@ -1,4 +1,5 @@
 const taskModel = require('../models/taskModel');
+const userModel = require('../models/userModel');
 const JOI = require('@hapi/joi');
 const { errorUtils } = require('./utils');
 
@@ -7,11 +8,14 @@ const taskSchema = JOI.object({
 	status: JOI.string().valid('pendente', 'em andamento', 'pronto').required(), 
 });
 
-const newTaskService = async (newTask, userId) => {
+const newTaskService = async (newTask, userId, email) => {
 	const { text, status } = newTask;
 
 	const validate = taskSchema.validate({ text, status });
 	if (validate.error) throw errorUtils(400, validate.error.message);
+
+	const userExists = await userModel.searchEmail(email);
+	if (!userExists) throw errorUtils(400, 'User does not exists');
 
 	const createTask = await taskModel.newTaskModel(newTask, userId);
 	return createTask;
