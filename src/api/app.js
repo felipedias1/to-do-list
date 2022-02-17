0c83c033b4fada8f2c179965503ceaca3be6dae0
Felipe Dias <felipeferreiradias1@gmail.com>
2022-02-17 04:22:07 -0300

    update: add dependency cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const userRoutes = require('../routes/userRoutes');
 const taskRoutes = require('../routes/taskRoutes');
 const loginRoutes = require('../routes/loginRoutes');
 const errorMiddleware = require('../middlewares/errorMiddleware');
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
